1e0e113800351bc9fc9a8411fb6ff2c022617e25
Vũ Khôi <vubakhoi3792@gmail.com>
2023-11-14 16:12:30 +0700

    front-latest product

--- components/Button.js
+++ components/Button.js
@@ -1,42 +1,51 @@
-import styled from "styled-components";
-import css from "styled-jsx/css";
+import styled,{css} from "styled-components";
+import { primary } from "@/lib/colors";
 
-
-const StyledButton = styled.button`
+export const ButtonStyle  = css`
 border:0;
 padding: 5px 15px;
 border-radius: 5px;
 cursor: pointer;
 display: inline-flex;
 align-items: center;
+text-decoration: none;
+font-family: 'Poppins' , sans-serif;
+font-weight: 500;
 svg{
     height: 16px;
     margin-right: 5px;
 }
+${props => props.white && !props.outline && css`
+    background-color: #fff;
+    color: #000;
+    `}
 ${props => props.white && props.outline && css`
-background-color: #fff;
-color: #000;
-`}
-${props => props.white && props.outline && css`
-background-color: transparent;
-color: #fff;
-border: 1px solid #fff;
-`}
+    background-color: transparent;
+    color: #fff;
+    border: 1px solid #fff;
+    `}
 
-${props => props.primary && css`
-background-color: #5542F6;
-border: 1px solid #5542f6;
-color:#fff;
+${props => props.primary && !props.outline && css`
+    background-color: ${primary};
+    border: 1px solid ${primary};
+    color:#fff;
+    `}
 
-
-`}
+${props => props.primary && props.outline && css`
+    background-color: transparent;
+    border: 1px solid ${primary};
+    color: ${primary};
+    `}
 ${props => props.size === 'l' && css`
-font-size:1.2rem;
-padding: 10px 20px;
-svg{
-    height: 20px;
-}
-`}
+    font-size:1.2rem;
+    padding: 10px 20px;
+    svg{
+        height: 20px;
+        }
+    `}
+`;
+const StyledButton = styled.button`
+    ${ButtonStyle}
 `;
 
 export default function Button({children,...rest}) {
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import Featured from "@/components/Featured";
 import Header from "@/components/Header";
-import { Product } from "@/models/Product";
-import { mongooseConnect } from "@/lib/mongoose";
+import Featured from "@/components/Featured";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
 
-export default function HomePage({product}){
-  console.log(product);
-  return(
+export default function HomePage({featuredProduct,newProducts}) {
+  return (
     <div>
       <Header />
-      <Featured product={product} />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
     </div>
   );
 }
@@ -16,8 +17,12 @@ export default function HomePage({product}){
 export async function getServerSideProps() {
   const featuredProductId = '640de2b12aa291ebdf213d48';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
-  return{
-    props:{product: JSON.parse(JSON.stringify(product))},
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
